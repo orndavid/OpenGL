77f27acc247e887a766e2e1e9d6ee9b298b603ff
orndavid <orn.david@gmail.com>
2020-06-26 09:06:37 +1000

    Changing namespaces

--- src/rtest.cpp
+++ src/rtest.cpp
@@ -1,24 +1,9 @@
 #include "gl_api.h"
 #include <iostream>
 #include <exception>
-#include <thread>
-
-void build_window()
-{
-  try{
-    Window nwind;
-
-  }catch(const std::runtime_error& e)
-    {
-      std::cerr << e.what() << std::endl;
-    }
-}
 
 int main(void)
 {
-  /*std::thread thr_class(build_window);
-  std::cout << "Continuing the main function \n";
-  thr_class.join();*/
   Window nwind;
   std::cout << "Finishing function \n";
   return 0;
