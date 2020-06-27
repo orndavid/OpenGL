4c273572062e597a8a407f6b12ff5c104db65c55
orndavid <orn.david@gmail.com>
2020-06-27 10:08:51 +1000

    Fixed example, now we have mix on two figures

--- src/tester2.cpp
+++ src/tester2.cpp
@@ -4,14 +4,15 @@
 #include "linmath.h" //Matrix computation in render()
 
 // static struct _verticies
-//   configuration  x, y, z, r, g, b;
-float vertices[4][7] =
+//   configuration  x, y, z, r, g, b, alph, tx, ty
+float vertices[4][9] =
   {
-   { -0.7f,  0.7f, 0.0f, 1.f, .0f, 0.f, 1.0f },//0
-   { -0.7f, -0.7f, 0.0f, 0.f, 1.f, 0.f, 1.0f },//1
-   {  0.7f, -0.7f,-0.0f, 0.f, 0.f, 1.f, 1.0f },//2
-   {  0.7f,  0.7f, 0.0f, 0.f, 1.f, 0.f, .1f } //3
+   { -0.9f, -0.9f, 0.0f, 1.f, 0.0f, 0.f, 1.0f, 0.0f, 0.0f},//0
+   {  0.9f, -0.9f, 0.0f, 0.f, 1.0f, 0.f, 1.0f, 1.0f, 0.0f},//1
+   {  0.9f,  0.9f,-0.0f, 0.f, 0.0f, 1.f, 1.0f, 1.0f, 1.0f},//2
+   { -0.9f,  0.9f, 0.0f, 0.f, 1.0f, 0.f, 0.1f, 0.0f, 1.0f} //3
   };
+
 
 unsigned int indicies[] =
   {
@@ -34,7 +35,7 @@ int main(void)
   { /* This is because we mut ensure that the buffers destructors get called before the final window destructor */
 
     //VertexBuffer<float> vbo(data, 5);
-  VertexBuffer vbo(vertices, 4*7*sizeof(float));
+  VertexBuffer vbo(vertices, 4*9*sizeof(float));
   IndexBuffer  ibo(indicies, 6);
   vbo.Bind();
   ibo.Bind();
@@ -42,18 +43,29 @@ int main(void)
 
   program.add_shader(fragment_shader);
   program.add_shader(vertex_shader);
-  program.Bind();
 
-  program.vap("vPos", 3, GL_FLOAT, 7*sizeof(float), (void*) 0);
-  program.vap("vCol", 4, GL_FLOAT, 7*sizeof(float), (void*) (sizeof(float)*3));
+  // Create texture mapping on GPU
+  Textures t_map(FIG_TYPE::PNG, "../Textures/brisbane-observations-map.png");
+  t_map.freeLocal();
+  Textures t_alex(FIG_TYPE::JPG, "../Textures/kids1.jpg");
+  t_alex.freeLocal();
+
+  program.Bind(); // We must remember to activate shaders before setting uniforms
+  program.Use();
+  // Define the uniforms in the shaders
+  program.uniformI("text1", 0);
+  program.uniformI("text2", 1);
+
+  program.vap("aPos", 3, GL_FLOAT, 9*sizeof(float), (void*) 0);
+  program.vap("aCol", 4, GL_FLOAT, 9*sizeof(float), (void*) (sizeof(float)*3));
+  program.vap("aTexCoord", 2, GL_FLOAT, 9*sizeof(float), (void*) (sizeof(float)*7));
 
   mat4x4 m, p, mvp;
   unsigned int mvp_location = program.uniform_loc("MVP");
   int height, width;
   float ratio;
   // Move all into main function
-
-  while(!nwin.close())
+    while(!nwin.close())
     {
       nwin.windowSize(&height, &width);
       nwin.clear();
@@ -62,15 +74,18 @@ int main(void)
       nwin.constantViewport();
 
       mat4x4_identity(m);
-      mat4x4_rotate_Z(m, m, -(float) glfwGetTime());
+      mat4x4_rotate_Z(m, m, -(float) glfwGetTime()/10);
+
       mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
       mat4x4_mul(mvp, p, m);
 
-      program.use();
+      program.Use();
       // This is a math specific call and is really specific to this vertex shader
       glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
       // Overwrite this in nwin
       //nwin.draw(GL_TRIANGLES, ibo.count(), GL_UNSIGNED_INT);
+      t_alex.Bind(0);
+      t_map.Bind(1);
       nwin.tDrawF(ibo);
       //      nwin.draw(GL_LINE_LOOP, ibo.count(), GL_UNSIGNED_INT);
 
